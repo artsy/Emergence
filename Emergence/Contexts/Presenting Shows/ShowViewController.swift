e15b0b076daf4c9a74e2959d1744efbb57baaac9
Orta Therox <orta.therox@gmail.com>
2015-09-24 11:21:32 +0100

    Initial work on an ORM from Transition -> Show

--- Emergence/Contexts/Presenting Shows/ShowViewController.swift
+++ Emergence/Contexts/Presenting Shows/ShowViewController.swift
@@ -1,5 +1,9 @@
 import UIKit
+import RxSwift
+import Hyperdrive
 import RxHyperdrive
+import Gloss
+import Representor
 
 class ShowViewController: UIViewController {
 
@@ -14,10 +18,41 @@ class ShowViewController: UIViewController {
         let network = appVC.context.network
         if let shows = network.root.transitions["show"] {
             let attributes = ["id": "4ea19ee97bab1a0001001908"]
-            network.request(shows, parameters: attributes).subscribeNext { representor in
-                print(representor)
+            network.request(shows, parameters: attributes).mapTransitionToObject(Show).subscribeNext { showObject in
+                let show = showObject as! Show
+                print(show.name)
             }
         }
+    }
+}
 
+enum ORMError : ErrorType {
+    case ORMNoRepresentor
+    case ORMNoData
+    case ORMCouldNotMakeObjectError
+}
+
+extension Observable {
+    func mapTransitionToObject(classType: Decodable.Type) -> Observable<Decodable> {
+
+        func resultFromJSON(object:[String: AnyObject], classType: Decodable.Type) -> Decodable? {
+            return classType.init(json: object)
+        }
+
+        return map { representor in
+            guard let rep = representor as? Representor<HTTPTransition> else {
+                throw ORMError.ORMNoRepresentor
+            }
+
+            if rep.attributes.count == 0 {
+                throw ORMError.ORMNoData
+            }
+
+            guard let obj = resultFromJSON(rep.attributes, classType: classType)  else {
+                throw ORMError.ORMCouldNotMakeObjectError
+            }
+
+            return obj
+        }
     }
 }
--- Emergence/Models/Artsy Model Protocols/Showable.swift
+++ Emergence/Models/Artsy Model Protocols/Showable.swift
@@ -3,4 +3,6 @@
 protocol Showable {
     var installShots: [Imageable] { get }
     var artworks: [Artworkable] { get }
+
+    var name: String { get }
 }
