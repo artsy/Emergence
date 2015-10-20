91e6f0b34ba78bd36515120dcab11e5f58e60fa3
Orta Therox <orta.therox@gmail.com>
2015-10-20 10:44:02 +0100

    Support showing a highlight for images which haven't loaded yet

---
+++ Emergence/Extensions/Apple/UIImageView+Artsy.swift
@@ -0,0 +1,35 @@
+//
+//  UIImageView+Artsy.swift
+//  Emergence
+//
+//  Created by Orta Therox on 20/10/2015.
+//  Copyright © 2015 Artsy. All rights reserved.
+//
+
+import UIKit
+
+class UIImageView_Artsy: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
